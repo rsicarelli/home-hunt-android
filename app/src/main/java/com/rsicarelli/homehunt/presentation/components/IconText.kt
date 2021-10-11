50c969695107076c151be67b49a9ddef78a7c0d6
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2021-10-11 11:12:19 +0200

    remove unused components

--- app/src/main/java/com/rsicarelli/homehunt/presentation/components/IconText.kt
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/components/IconText.kt
@@ -1,7 +1,6 @@
 package com.rsicarelli.homehunt.presentation.components
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.IntegerRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
@@ -10,7 +9,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
