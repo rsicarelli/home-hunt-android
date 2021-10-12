ce91f169331b58432f804db053a0c532d567be25
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2021-10-12 17:53:23 +0200

    extract and create preview for filter fab

--- app/src/main/java/com/rsicarelli/homehunt/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/home/HomeScreen.kt
@@ -1,35 +1,21 @@
 package com.rsicarelli.homehunt.presentation.home
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.FloatingActionButtonDefaults
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.rsicarelli.homehunt.R
-import com.rsicarelli.homehunt.core.model.ScaffoldDelegate
 import com.rsicarelli.homehunt.presentation.components.CircularIndeterminateProgressBar
 import com.rsicarelli.homehunt.presentation.components.EmptyContent
 import com.rsicarelli.homehunt.presentation.components.LifecycleEffect
+import com.rsicarelli.homehunt.presentation.home.components.FilterFab
 import com.rsicarelli.homehunt.presentation.home.components.PropertyList
 import com.rsicarelli.homehunt.ui.composition.LocalScaffoldDelegate
 import com.rsicarelli.homehunt.ui.navigation.Screen
-import com.rsicarelli.homehunt.ui.theme.BottomBarSize
-import com.rsicarelli.homehunt.ui.theme.ElevationSize
-import com.rsicarelli.homehunt.ui.theme.Size_Small
-import com.rsicarelli.homehunt.ui.theme.rally_blue_700
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -85,31 +71,6 @@ private fun HomeContent(
 
         CircularIndeterminateProgressBar(state.progressBarState)
     }
-
 }
 
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-private fun FilterFab(isScrollInProgress: Boolean, onClick: () -> Unit) {
-    AnimatedVisibility(
-        visible = !isScrollInProgress,
-        enter = expandVertically(expandFrom = Alignment.Top),
-        exit = shrinkVertically(shrinkTowards = Alignment.Top)
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(bottom = BottomBarSize, end = Size_Small)
-        ) {
-            FloatingActionButton(
-                onClick = onClick,
-                elevation = FloatingActionButtonDefaults.elevation(ElevationSize),
-                backgroundColor = rally_blue_700
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_round_filter),
-                    contentDescription = stringResource(id = R.string.filter)
-                )
-            }
-        }
-    }
-}
+
---
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/home/components/FilterFab.kt
@@ -0,0 +1,53 @@
+package com.rsicarelli.homehunt.presentation.home.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.rsicarelli.homehunt.R
+import com.rsicarelli.homehunt.ui.theme.*
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun FilterFab(isScrollInProgress: Boolean, onClick: () -> Unit) {
+    AnimatedVisibility(
+        visible = !isScrollInProgress,
+        enter = expandVertically(expandFrom = Alignment.Top),
+        exit = shrinkVertically(shrinkTowards = Alignment.Top)
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(bottom = BottomBarSize, end = Size_Small)
+        ) {
+            FloatingActionButton(
+                onClick = onClick,
+                elevation = FloatingActionButtonDefaults.elevation(ElevationSize),
+                backgroundColor = rally_blue_700
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_round_filter),
+                    contentDescription = stringResource(id = R.string.filter)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun FilterFabPreview() {
+    HomeHuntTheme(isPreview = true) {
+        FilterFab(isScrollInProgress = false, onClick = {})
+    }
+}
