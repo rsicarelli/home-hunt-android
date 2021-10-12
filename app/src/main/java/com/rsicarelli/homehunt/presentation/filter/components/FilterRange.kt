b708ece83fe594fe0b01340a91511a6e4afcfc32
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2021-10-12 14:48:12 +0200

    added preview for filter range

--- app/src/main/java/com/rsicarelli/homehunt/presentation/filter/components/FilterRange.kt
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/filter/components/FilterRange.kt
@@ -1,15 +1,16 @@
 package com.rsicarelli.homehunt.presentation.filter.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.rsicarelli.homehunt.ui.theme.DividerSize
-import com.rsicarelli.homehunt.ui.theme.Size_X_Small
-import com.rsicarelli.homehunt.ui.theme.Size_Small
-import com.rsicarelli.homehunt.ui.theme.Size_Regular
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.rsicarelli.homehunt.R
+import com.rsicarelli.homehunt.ui.theme.*
 
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -46,4 +47,23 @@ fun FilterRange(
     Spacer(modifier = Modifier.height(Size_Small))
 
     Divider(thickness = DividerSize)
+}
+
+@Composable
+@Preview
+private fun FilterRangePreview() {
+    HomeHuntTheme {
+        val range = 400f..1500f
+        val valueRange = 0f..3000f
+
+        Column {
+            FilterRange(
+                title = "A filter range",
+                range = range,
+                valueRange = valueRange,
+                rangeText = "${range.start.toInt()} - ${range.endInclusive.toInt()}",
+                onValueChange = { }
+            )
+        }
+    }
 }
