fc2b738f4d811fc8a1e486863919b2668565abff
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2021-10-12 14:11:11 +0200

    remove import from filter screen

--- app/src/main/java/com/rsicarelli/homehunt/presentation/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/favourites/FavouritesScreen.kt
@@ -7,7 +7,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.rsicarelli.homehunt.R
 import com.rsicarelli.homehunt.core.model.ScaffoldDelegate
-import com.rsicarelli.homehunt.domain.model.Property
 import com.rsicarelli.homehunt.domain.model.toTag
 import com.rsicarelli.homehunt.presentation.components.AppScaffold
 import com.rsicarelli.homehunt.presentation.components.EmptyContent
