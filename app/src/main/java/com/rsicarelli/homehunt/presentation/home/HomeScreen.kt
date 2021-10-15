1ea6490244855c4cef4435fdf106635257d64b86
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2021-10-15 11:31:45 +0200

    rollback lifecyle changes

--- app/src/main/java/com/rsicarelli/homehunt/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/rsicarelli/homehunt/presentation/home/HomeScreen.kt
@@ -34,7 +34,7 @@ fun HomeScreen(
     val stateFlowLifecycleAware = viewModel.rememberOnLifecycle {
         viewModel.init().flowWithLifecycle(
             lifecycle = it.lifecycle,
-            minActiveState = Lifecycle.State.RESUMED
+            minActiveState = Lifecycle.State.STARTED
         )
     }
 
